2cb2e39481fa4000098e755dd175faf27bdc399d
Maulidaa <maulidaallya06@gmail.com>
2025-08-05 09:49:20 +0700

    feat: Add Book and Category models

--- app/Book.php
+++ app/Book.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    protected $fillable = ['title', 'author', 'isbn', 'description', 'published_date', 'category_id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
