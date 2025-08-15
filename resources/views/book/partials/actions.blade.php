974c9733778fdbc8da840de94ef30d75aebea93f
Maulidaa <maulidaallya06@gmail.com>
2025-08-15 16:51:07 +0800

    feat: Enhance action buttons with icons and titles for better accessibility

--- resources/views/book/partials/actions.blade.php
+++ resources/views/book/partials/actions.blade.php
@@ -4,11 +4,20 @@
 @endphp
 
 @if($isAdmin)
-    <a href="{{ route('books.edit', $book->id) }}" class="btn btn-sm btn-primary">Edit</a>
+    <a href="{{ route('books.edit', $book->id) }}" class="btn btn-sm btn-primary" title="Edit">
+        <i class="icon-pencil7 mr-2"></i>
+        <span class="d-none d-sm-inline">Edit</span>
+    </a>
 @endif
 
 @if(auth()->check())
-    <a href="{{ route('books.chapters', $book->id) }}" class="btn btn-sm btn-info">Detail</a>
+    <a href="{{ route('books.chapters', $book->id) }}" class="btn btn-sm btn-info" title="Detail">
+        <i class="icon-eye mr-2"></i>
+        <span class="d-none d-sm-inline">Detail</span>
+    </a>
 @else
-    <a href="{{ $loginRoute }}" class="btn btn-sm btn-info">Detail</a>
+    <a href="{{ $loginRoute }}" class="btn btn-sm btn-info" title="Detail">
+        <i class="icon-eye mr-1"></i>
+        <span class="d-none d-sm-inline">Detail</span>
+    </a>
 @endif
--- resources/views/chapters/partials/actions.blade.php
+++ resources/views/chapters/partials/actions.blade.php
@@ -1,15 +1,22 @@
-
 <?php 
 $isAdmin = auth()->user()->role_id==1;
 $isAuthor = auth()->user()->id == $chapter->author_id;
 if ($isAdmin || $isAuthor): ?>
-    <a href="{{ route('chapters.download_pdf', ['id' => $chapter->book_id, 'chapterId' => $chapter->id]) }}" class="btn btn-sm btn-success">Download</a>
-    <a href="{{ route('chapters.edit', array('id' => $chapter->book_id, 'chapterId' => $chapter->id)) }}" class="btn btn-sm btn-primary">Edit</a>
+    <a href="{{ route('chapters.download_pdf', ['id' => $chapter->book_id, 'chapterId' => $chapter->id]) }}" class="btn btn-sm btn-success mr-1" title="Download">
+        <i class="icon-file-pdf"></i>
+    </a>
+    <a href="{{ route('chapters.edit', ['id' => $chapter->book_id, 'chapterId' => $chapter->id]) }}" class="btn btn-sm btn-primary mr-1" title="Edit">
+        <i class="icon-pencil7"></i>
+    </a>
     <form action="{{ route('chapters.destroy', ['id' => $chapter->book_id, 'chapterId' => $chapter->id]) }}" method="POST" style="display:inline;">
         @csrf
         @method('DELETE')
-        <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Delete this chapter?')">Delete</button>
+        <button type="submit" class="btn btn-sm btn-danger mr-1" onclick="return confirm('Delete this chapter?')" title="Delete">
+            <i class="icon-trash"></i>
+        </button>
     </form>
 <?php endif; ?>
-<a href="{{ route('chapters.show', ['id' => $chapter->book_id, 'chapterId' => $chapter->id]) }}" class="btn btn-sm btn-info">Detail</a>
+<a href="{{ route('chapters.show', ['id' => $chapter->book_id, 'chapterId' => $chapter->id]) }}" class="btn btn-sm btn-info" title="Detail">
+    <i class="icon-eye"></i>
+</a>
 
